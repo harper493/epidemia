112e405b1fe85178c2cffffd080c0f77fdae5309
john <john-a-harper@hotmail.com>
2020-04-19 20:16:18 -0700

    Add support for cartesian product of ranges

--- sensitivity.py
+++ sensitivity.py
@@ -1,20 +1,26 @@
 from utility import *
+from math import fsum
+import re
 
 class sensitivity():
 
     class one_param() :
 
         def __init__(self, spec: str):
-            self.pname, self.text = spec.split(':', 1)
-            details = self.text
-            if ':' in details :
-                if '*' in details:
-                    self.mult = True
-                    details = details.replace('*', ':')
-                else:
-                    self.mult = False
-                self.values = None
-                try :
+            try :
+                m = re.match(r'^([+;])([^:]+):(.*)', spec)
+                if not m :
+                    raise ValueError
+                prod, self.pname, self.text = m.group(1, 2, 3)
+                self.product = (prod=='+')
+                details = self.text
+                if ':' in details :
+                    if '*' in details:
+                        self.mult = True
+                        details = details.replace('*', ':')
+                    else:
+                        self.mult = False
+                    self.values = None
                     r = details.split(':') + [None, None]
                     self.start, self.step, self.stop, stopper = r[:4]
                     if stopper is not None :
@@ -22,21 +28,17 @@ class sensitivity():
                     self.start, self.step = number(self.start), number(self.step)
                     if self.stop is not None :
                         self.stop = number(self.stop)
-                except IndexError:
-                    raise ValueError(f"Incorrect parameter range '{spec}'")
-            else :
-                self.values = []
-                for v in details.split(',') :
-                    vv, m = (v.split('*') + [1])[:2]
-                    self.values += [number(vv)] * number(m)
-                self.start = 0
-                self.index = 0
-                self.step = 1
-                self.stop = None
-            if self.values :
-                self.value = self.values[0]
-            else :
-                self.value = self.start
+                else :
+                    self.values = []
+                    for v in details.split(',') :
+                        vv, m = (v.split('*') + [1])[:2]
+                        self.values += [number(vv)] * number(m)
+                    self.start = 0
+                    self.step = 1
+                    self.stop = None
+                self.reset()
+            except (IndexError, ValueError):
+                raise ValueError(f"Incorrect parameter range '{spec}'")
 
         def __str__(self):
             return self.text
@@ -44,13 +46,20 @@ class sensitivity():
         def get(self):
             return (self.pname, self.value)
 
+        def reset(self):
+            self.index = 0
+            if self.values:
+                self.value = self.values[0]
+            else:
+                self.value = self.start
+
         def next(self) -> bool:
             """
             Advance to the next value.
             :return: True iff the value is within the stated range.
             """
+            self.index += 1
             if self.values :
-                self.index += 1
                 try :
                     self.value = self.values[self.index]
                 except IndexError :
@@ -59,7 +68,7 @@ class sensitivity():
             if self.mult :
                 self.value *= self.step
             else :
-                self.value += self.step
+                self.value = self.start + self.step * self.index#fsum([self.start] + [self.step] * self.index)
             if self.stop is None :
                 return True
             elif self.step>0 :
@@ -71,7 +80,11 @@ class sensitivity():
             return self.values or self.stop is not None
 
     def __init__(self, params, max_iterations: int=100):
-        self.ranges = [ sensitivity.one_param(p) for p in params.split(';') ]
+        r = ['+'] + re.split(r'([+;])', params)
+        if '' in r :
+            raise ValueError(f"Syntax error in sensitivity description: '{params}'")
+        r2 = [ m+t for m,t in zip(r[::2], r[1::2])]
+        self.ranges = [ sensitivity.one_param(p) for p in r2 ]
         self.named_ranges = { r.pname : r for r in self.ranges }
         self.max_iterations = max_iterations
         if not self.ranges[0].will_stop() :
@@ -133,11 +146,22 @@ class sensitivity():
 
     def __iter__(self):
         good = True
-        i = 0
-        while (good and i < self.max_iterations):
+        count = 0
+        while (count < self.max_iterations):
             yield [r.get() for r in self.ranges]
-            good = [r.next() for r in self.ranges][0]
-            i += 1
+            top = len(self.ranges)
+            for ii, r in enumerate(reversed(self.ranges)) :
+                i = len(self.ranges) - 1 - ii
+                if r.product :
+                    g = [ r2.next() for r2 in self.ranges[i:top] ][0]
+                    if g :
+                        break
+                    for r in self.ranges[i:top] :
+                        r.reset()
+                    top = i
+            else :
+                break
+            count += 1
 
 def _test_one(s) :
     sens = sensitivity(s)
@@ -147,6 +171,7 @@ def _test_one(s) :
     print('\n')
 
 if __name__=='__main__' :
+    _test_one('foo:1,2+bah:10,11+bim:100,101;boo:1000')
     _test_one('foo:1:0.1:2')
     _test_one('foo:1*2:16')
     _test_one('foo:1,2,3,4')
@@ -154,3 +179,4 @@ if __name__=='__main__' :
     _test_one('foo:1,2*10,3')
     _test_one('foo:1:1:10;bah:2:2')
     _test_one('foo:1,2,3,4,5;bah:2,4')
+    _test_one('foo:1:1:3+bah:10:1:12')
